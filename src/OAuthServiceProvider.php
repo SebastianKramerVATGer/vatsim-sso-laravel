879f84678ab4cab75b8cb447ba0365951e47125e
Sebastian <sebastian@jskramer.de>
2019-09-29 19:04:12 +0200

    adjusted arr helpers to reflect changes within laravel v6+

--- src/OAuthServiceProvider.php
+++ src/OAuthServiceProvider.php
@@ -1,66 +1,68 @@
-<?php namespace Vatsim\OAuthLaravel;
+<?php
 
-use Illuminate\Support\ServiceProvider;
+namespace Vatsim\OAuthLaravel;
+
 use Vatsim\OAuth\SSO;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
 
-class OAuthServiceProvider extends ServiceProvider {
+class OAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        $app = $this->app;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot ()
-	{
-		$app = $this->app;
+        $this->publishes(
+            [
+                __DIR__.'/config.php' => config_path('vatsim-sso.php'),
+            ],
+            'config'
+        );
+    }
 
-		$this->publishes([
-			__DIR__ . '/config.php' => config_path('vatsim-sso.php'),
-		], 'config');
-	}
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            'vatsimoauth',
+            function ($app) {
+                $config = $app['config']['vatsim-sso'];
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register ()
-	{
-		$this->app->singleton('vatsimoauth', function ( $app )
-		{
-			$config = $app['config']['vatsim-sso'];
+                // Make sure we don't crash when we did not publish the config file
+                if (is_null($config)) {
+                    $config = [];
+                }
 
-			// Make sure we don't crash when we did not publish the config file
-			if ( is_null($config) )
-			{
-				$config = [];
-			}
+                return new SSO(
+                    Arr::get($config, 'base'), // base
+                    Arr::get($config, 'key'), // key
+                    Arr::get($config, 'secret'), // secret
+                    Arr::get($config, 'method'), // method
+                    Arr::get($config, 'cert') // certificate
+                );
+            }
+        );
+    }
 
-			return new SSO(
-				array_get($config, 'base'), // base
-				array_get($config, 'key'), // key
-				array_get($config, 'secret'), // secret
-				array_get($config, 'method'), // method
-				array_get($config, 'cert') // certificate
-			);
-		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides ()
-	{
-		return [SSO::class];
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [SSO::class];
+    }
 }
